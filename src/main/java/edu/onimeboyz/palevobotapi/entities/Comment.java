5419411e3440551d616b75de13e00917b0959520
ilyinalex <ilinoleksiii@gmail.com>
2018-12-05 01:05:14 +0200

    Model Comment is implemented

--- src/main/java/edu/onimeboyz/palevobotapi/entities/Comment.java
+++ src/main/java/edu/onimeboyz/palevobotapi/entities/Comment.java
@@ -3,13 +3,23 @@ package edu.onimeboyz.palevobotapi.entities;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "comments")
 public class Comment {
+
+    private String content;
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "palevo_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Palevo palevo;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
