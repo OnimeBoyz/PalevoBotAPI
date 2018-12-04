ca647674be79da211ed769d76ee1cfcbbc3e1c37
ilyinalex <ilinoleksiii@gmail.com>
2018-12-05 00:22:48 +0200

    PalevoController is added

---
+++ src/main/java/edu/onimeboyz/palevobotapi/controllers/PalevoController.java
@@ -0,0 +1,10 @@
+package edu.onimeboyz.palevobotapi.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class PalevoController {
+}
