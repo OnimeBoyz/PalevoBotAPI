bcfa0d479b113599e10e8b11346baa20f84e3efc
ilyinalex <ilinoleksiii@gmail.com>
2018-12-04 20:44:38 +0200

    application.properties is added

--- src/main/java/edu/onimeboyz/palevobotapi/Main.java
+++ src/main/java/edu/onimeboyz/palevobotapi/Main.java
@@ -1,4 +1,18 @@
 package edu.onimeboyz.palevobotapi;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
 public class Main {
+    public static void main(String[] args) {
+        startAPI(args);
+    }
+
+    private static void startAPI(String[] args){
+        SpringApplication.run(Main.class, args);
+    }
 }
---
+++ src/test/java/edu/onimeboyz/palevobotapi/controllers/UserControllerTest.java
@@ -0,0 +1,7 @@
+package edu.onimeboyz.palevobotapi.controllers;
+
+import static org.junit.Assert.*;
+
+public class UserControllerTest {
+
+}
