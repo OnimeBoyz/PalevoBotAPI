e2ec62ababef1d753be5ba8c85be442fafb0909e
ilyinalex <ilinoleksiii@gmail.com>
2018-12-04 22:51:49 +0200

    main, changes with dependencies and some other refactoring ;)

--- src/main/java/edu/onimeboyz/palevobotapi/controllers/UserController.java
+++ src/main/java/edu/onimeboyz/palevobotapi/controllers/UserController.java
@@ -10,42 +10,35 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-    @PostMapping("/users")
+    @PostMapping
     public User createUser(@Valid @RequestBody User user) {
         return userRepository.save(user);
     }
 
-    @GetMapping("/users/{id}")
-    public User getUserById(@PathVariable(value = "id") Integer userId) {
-        return userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("Wrong id"));
-    }
-
-    @GetMapping("/users/{username}")
-    public User getUserById(@PathVariable(value = "username") String username) {
+    @GetMapping("/{username}")
+    public User getUserByName(@PathVariable(value = "username") String username) {
         return userRepository.findAll().stream()
                 .filter(user -> user.getUsername().trim().equals(username.trim()))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException("Wrong username"));
     }
 
-    @PutMapping("/users/{id}")
-    public User updateNote(@PathVariable(value = "id") Integer userId,
+    @PutMapping("/{username}")
+    public User updateNote(@PathVariable(value = "username") String username,
                               @Valid @RequestBody User userDetails) {
 
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("Wrong id"));
+        User user = getUserByName(username);
 
         user.setUsername(userDetails.getUsername());
         user.setRating(userDetails.getRating());
@@ -54,17 +47,11 @@ public class UserController {
     }
 
 
-    @DeleteMapping("/users/{id}")
-    public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Integer userId) {
-        System.out.println(userRepository.findAll().size());
-        for (User user: userRepository.findAll()) {
-            System.out.println(user.getId());
-        }
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("Wrong id"));
+    @DeleteMapping("/username")
+    public ResponseEntity<?> deleteNote(@PathVariable(value = "username") String username) {
 
+        User user = getUserByName(username);
         userRepository.delete(user);
-
         return ResponseEntity.ok().build();
     }
 
--- src/test/java/edu/onimeboyz/palevobotapi/controllers/UserControllerTest.java
+++ src/test/java/edu/onimeboyz/palevobotapi/controllers/UserControllerTest.java
@@ -3,5 +3,5 @@ package edu.onimeboyz.palevobotapi.controllers;
 import static org.junit.Assert.*;
 
 public class UserControllerTest {
-
+    //TODO
 }
