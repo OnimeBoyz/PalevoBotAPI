c3e24e31c26b9a1923fd629a0447b31047c07c89
ilyinalex <ilinoleksiii@gmail.com>
2018-12-05 01:56:57 +0200

    PalevoController().getAllPalevos()                   .createPalevo() are implemented

--- src/main/java/edu/onimeboyz/palevobotapi/controllers/PalevoController.java
+++ src/main/java/edu/onimeboyz/palevobotapi/controllers/PalevoController.java
@@ -1,10 +1,29 @@
 package edu.onimeboyz.palevobotapi.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.onimeboyz.palevobotapi.entities.Palevo;
+import edu.onimeboyz.palevobotapi.entities.User;
+import edu.onimeboyz.palevobotapi.repositories.PalevoRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/palevos")
 public class PalevoController {
+    PalevoRepository palevoRepository;
+
+    @GetMapping
+    public List<Palevo> getAllPalevos() {
+        return palevoRepository.findAll();
+    }
+
+    @PostMapping
+    public Palevo createPalevo(@Valid @RequestBody Palevo palevo) {
+        return palevoRepository.save(palevo);
+    }
+
+
 }
