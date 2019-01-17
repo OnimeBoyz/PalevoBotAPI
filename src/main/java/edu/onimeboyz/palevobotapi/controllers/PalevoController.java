33d8bf4194dde5a45f5bd9051c4876f97d06972f
Oleg <Oleg58807@ukr.net>
2019-01-17 23:31:18 +0200

    Updated palevo controller

--- src/main/java/edu/onimeboyz/palevobotapi/controllers/PalevoController.java
+++ src/main/java/edu/onimeboyz/palevobotapi/controllers/PalevoController.java
@@ -1,10 +1,34 @@
 package edu.onimeboyz.palevobotapi.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.onimeboyz.palevobotapi.entities.Palevo;
+import edu.onimeboyz.palevobotapi.entities.User;
+import edu.onimeboyz.palevobotapi.repositories.PalevoRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/palevos")
 public class PalevoController {
+    PalevoRepository palevoRepository;
+
+    @GetMapping
+    public List<Palevo> getAllPalevos() {
+        return palevoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Palevo getPalevoById(@PathVariable Integer id) {
+        return palevoRepository.findById(id).get();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePalevo(@PathVariable(value = "id") Integer palevoId) {
+        Palevo palevo = palevoRepository.findById(palevoId).get();
+
+        palevoRepository.delete(palevo);
+        return ResponseEntity.ok().build();
+    }
 }
